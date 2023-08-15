99ae9824ac6ffc7cc13987368b9df741621dd303
Sébastien <svidal1@free.fr>
2023-08-15 03:30:22 +0200

    modernize tools

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,93 +4,91 @@
 // deps
 
 	// natives
-	const { join } = require("path");
-
-	// externals
-	require("colors");
+	const { join } = require("node:path");
 
 	// locals
 	const checker = require(join(__dirname, "..", "lib", "main.js"));
-	const getFormatedTime = require(join(__dirname, "..", "lib", "utils", "getFormatedTime.js"));
 
 // consts
 
-	const options = {};
+	const ARGS = (0, process).argv.slice(2, (0, process).argv.length);
 
 // module
 
-let file = "";
-for (let i = 2, l = (0, process).argv.length; i < l; ++i) {
+Promise.resolve().then(() => {
 
-	switch ((0, process).argv[i]) {
+	const errors = [];
+	let file = "";
+	const options = {};
 
-		case "--fail-at-major":
-			options.failAtMajor = true;
-		break;
-		case "--no-fail-at-major":
-			options.failAtMajor = false;
-		break;
+		ARGS.forEach((arg, i) => {
 
-		case "--fail-at-minor":
-			options.failAtMinor = true;
-		break;
-		case "--no-fail-at-minor":
-			options.failAtMinor = false;
-		break;
+			switch ((0, process).argv[i]) {
 
-		case "--fail-at-patch":
-			options.failAtPatch = true;
-		break;
-		case "--no-fail-at-patch":
-			options.failAtPatch = false;
-		break;
+				case "--fail-at-major":
+					options.failAtMajor = true;
+				break;
+				case "--no-fail-at-major":
+					options.failAtMajor = false;
+				break;
 
-		case "--dev":
-			options.dev = true;
-		break;
-		case "--no-dev":
-			options.dev = false;
-		break;
+				case "--fail-at-minor":
+					options.failAtMinor = true;
+				break;
+				case "--no-fail-at-minor":
+					options.failAtMinor = false;
+				break;
 
-		case "--file":
+				case "--fail-at-patch":
+					options.failAtPatch = true;
+				break;
+				case "--no-fail-at-patch":
+					options.failAtPatch = false;
+				break;
 
-			if (i + 1 < l) {
-				file = String((0, process).argv[i + 1]); ++i;
+				case "--dev":
+					options.dev = true;
+				break;
+				case "--no-dev":
+					options.dev = false;
+				break;
+
+				case "--file":
+
+					if (i + 1 < l) {
+						file = String((0, process).argv[i + 1]); ++i;
+					}
+
+				break;
+
+				case "--console":
+					options.console = true;
+				break;
+				case "--no-console":
+					options.console = false;
+				break;
+
+				default:
+					errors.push(new RangeError("Unknown \"" + String(arg) + "\" argument"));
+				break;
+
 			}
 
-		break;
+		});
 
-		case "--console":
-			options.console = true;
-		break;
-		case "--no-console":
-			options.console = false;
-		break;
+	return errors.length ? Promise.reject(new Error(errors.join(EOL))) : checker("" === file ? join((0, process).cwd(), "package.json") : file, options).then((valid) => {
 
-		default:
-
-			(0, console).log("");
-			(0, console).error(getFormatedTime(), ("unknown \"" + String((0, process).argv[i]) + "\" argument").red);
-
-			(0, process).exitCode = 1;
-
-		break;
-
-	}
-
-}
-
-if (!(0, process).exitCode) {
-
-	checker("" === file ? join((0, process).cwd(), "package.json") : file, options).then((valid) => {
 		(0, process).exitCode = valid ? 0 : 2;
-	}).catch((err) => {
-
-		(0, console).log("");
-		(0, console).error(getFormatedTime(), (err.message ? err.message : err).red);
-
-		(0, process).exitCode = 1;
+		(0, process).exit(valid ? 0 : 2);
 
 	});
 
-}
+}).catch((err) => {
+
+	(0, console).log("");
+	(0, console).error(err.message ? err.message : err);
+
+	(0, process).exitCode = 1;
+	(0, process).exit(1);
+
+});
